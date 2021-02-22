9d0319370360b3df89a461a9484e072867770ad9
makroz <makroz@hotmail.com>
2021-02-22 15:12:44 -0400

    refactorizado de ordersController y de la migracion para aumentar datos de captura

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Orders;
 use App\Models\Products;
-use Illuminate\Http\Request;
 use Dnetix\Redirection\PlacetoPay;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 
 class OrdersController extends Controller
@@ -38,12 +38,21 @@ class OrdersController extends Controller
      */
     public function store(Request $request)
     {
-        $carrito = ['id' => 0, 'quantity' => 0];
+        
+        if (!$request->validate([
+            'customer_email'  => 'required|email',
+            'customer_name'   => 'required',
+            'customer_mobile' => 'required'
+        ])){
+            return Redirect::route('carritoListar')->with(['status' => 'Error de Campos']);
+        }
+
         if (session()->has('carrito')) {
             $carrito = session('carrito');
         } else {
             return Redirect::route('carritoListar');
         }
+
         $products = Products::find($carrito['id']);
 
         $placetopay = new PlacetoPay([
@@ -56,33 +65,39 @@ class OrdersController extends Controller
             ],
         ]);
 
-        $amount=$carrito['quantity'] * $products->price;
-        $reference = md5(date('YmdHis').'evertec');
+        $amount    = $carrito['quantity'] * $products->price;
+        $reference = md5(date('YmdHis') . 'evertec');
 
-        $requeste  = [
+        $requeste = [
+            'buyer'    => [
+                'name'   => $request->customer_name,
+                'email'   => $request->customer_email,
+                'mobile'   => $request->customer_mobile
+            ],
             'payment'    => [
                 'reference'   => $reference,
-                'description' => $products->name." x ".$carrito['quantity'],
+                'description' => $products->name . " x " . $carrito['quantity'],
+                'allowPartial'=> false,
                 'amount'      => [
                     'currency' => 'USD',
                     'total'    => $amount,
                 ],
             ],
             'expiration' => date('c', strtotime('+2 days')),
-            'returnUrl'  => 'http://evertec.test/response/' . $reference,
+            'returnUrl'  => getenv('APP_URL') . "/response/{$reference}",
             'ipAddress'  => $request->ip(),
             'userAgent'  => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
         ];
 
         try {
             $response = $placetopay->request($requeste);
-        }catch (\Exception $e) {
-            return Redirect::route('carritoListar')->with(['status'=>$e->getMessage()]);
+        } catch (\Exception $e) {
+            return Redirect::route('carritoListar')->with(['status' => $e->getMessage()]);
         }
 
         if ($response->isSuccessful()) {
-            $requestId=$response->requestId;
-            $processUrl=$response->processUrl;
+            $requestId               = $response->requestId;
+            $processUrl              = $response->processUrl;
             $orders                  = new Orders();
             $orders->customer_name   = $request->customer_name;
             $orders->customer_email  = $request->customer_email;
@@ -93,12 +108,17 @@ class OrdersController extends Controller
             $orders->status          = "CREATED";
             $orders->request_id      = $requestId;
             $orders->process_url     = $processUrl;
-            $orders->reference     = $reference;
+            $orders->reference       = $reference;
             $orders->save();
-            session()->forget('carrito');
+            session('customer',[
+            'email'  => $request->customer_email,
+            'name'   => $request->customer_name,
+            'mobile' => $request->customer_mobile
+            ]);
+        
             return Redirect::to($response->processUrl());
         } else {
-            return Redirect::route('carritoListar')->with(['status'=>$response->status()->message()]);
+            return Redirect::route('carritoListar')->with(['status' => $response->status()->message()]);
         }
     }
 
@@ -106,8 +126,9 @@ class OrdersController extends Controller
      * Intercept responde.
      *
      */
-    public function responseGet(Request $request,$reference)
+    public function responseGet(Request $request, $reference)
     {
+        
         $placetopay = new PlacetoPay([
             'login'   => config('dnetix.login'),
             'tranKey' => config('dnetix.trankey'),
@@ -118,18 +139,29 @@ class OrdersController extends Controller
             ],
         ]);
 
-        $order=Orders::where('reference',$reference)->first();
+        $order    = Orders::where('reference', $reference)->first();
         $response = $placetopay->query($order->request_id);
-
+        //dd($response);
         if ($response->isSuccessful()) {
             if ($response->status()->isApproved()) {
-                $order->status='PAYED';
-                $order->request_status='PAYED';
-                $order->request_message=$response->status()->message();
-                $order->save();
+                $order->status = 'PAYED';
+                session()->forget('carrito');
             }
+            if ($response->status()->isRejected()) {
+                $order->status = 'REJECTED';
+            }
+            if (!empty($response->payment)){
+                $order->request_currency = $response->payment[0]->amount()->to()->currency();
+                $order->request_amount = $response->payment[0]->amount()->to()->total();
+                $order->request_paymentMethodName = $response->payment[0]->paymentMethodName();
+            }
+            $order->request_status  = $response->status()->status();
+            $order->request_message = $response->status()->message();
+            $order->request_date = strftime("%F %X",strtotime($response->status()->date()));
+            $order->save();
+            return Redirect::route('misordenes')->with(['status' => $response->status()->message(),'request_status'=>$response->status()->status()]);
         } else {
-            print_r($response->status()->message() . "\n");
+            return Redirect::route('products')->with(['status' => $response->status()->message()]);
         }
     }
 }
--- database/migrations/2021_02_20_224822_update_orders_table.php
+++ database/migrations/2021_02_20_224822_update_orders_table.php
@@ -22,10 +22,13 @@ class UpdateOrdersTable extends Migration
             $table->string('reference', 32);
             $table->string('request_message', 200);
             $table->string('request_status', 10);
-            $table->decimal('request_amount');
-            $table->string('request_currency', 4);
+            $table->decimal('request_amount')->nullable();
+            $table->string('request_currency', 4)->nullable();
+            $table->decimal('request_factor')->nullable();
+            $table->string('request_paymentMethodName',20)->nullable();
             $table->integer('products_id')->unsigned();
             $table->foreign('products_id')->references('id')->on('products');
+            $table->index('reference');
         });
     }
 
@@ -48,6 +51,8 @@ class UpdateOrdersTable extends Migration
                 'request_status',
                 'request_amount',
                 'request_currency',
+                'request_paymentMethodName',
+                'request_factor',
                 'products_id'
             ]);
         });
